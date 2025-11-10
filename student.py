58d348c18757c4b792e33e70904b0e10e2b7c22d
sharanabasavas546 <sharanabasavas546@gmail.com>
2025-11-10 11:43:56 +0530

    Add student.py to handle command line input

---
+++ student.py
@@ -0,0 +1,15 @@
+import sys
+if len(sys.argv) !=3:
+script_name=sys.argv[0]
+name=sys.argv[1]
+rollno=sys.argv[2]
+print("user provide input value:")
+else:
+script_name=sys.argv[0]
+name="sharan"
+rollno="109"
+print("no input given -using default value:")
+
+print("script name:",script_name)
+print("student name:",name)
+print("roll number:",rollno)
